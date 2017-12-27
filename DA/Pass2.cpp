8e67d64cc700e2694e49697bf85d7ce2f77ede98
hassaans <18100222@lums.edu.pk>
2017-12-27 14:09:48 +0500

    commented DICE

--- DA/Pass2.cpp
+++ DA/Pass2.cpp
@@ -255,96 +255,96 @@ size_t hamming_distance(std::vector<Instruction*> v1, std::vector<Instruction*> 
 	return dist;
 }
 
-int dice_match(std::vector<Instruction*> string1, std::vector<Instruction*> string2)
-{
-    struct pairs
-            {
-            char chars[2];
-            struct pairs *next;
-            };
-    struct pairs *string1head = NULL;
-    struct pairs *string1current;
-    struct pairs *string1previous;
-    struct pairs *string2head = NULL;
-    struct pairs *string2current;
-    struct pairs *string2previous;
+// int dice_match(std::vector<Instruction*> string1, std::vector<Instruction*> string2)
+// {
+//     struct pairs
+//             {
+//             char chars[2];
+//             struct pairs *next;
+//             };
+//     struct pairs *string1head = NULL;
+//     struct pairs *string1current;
+//     struct pairs *string1previous;
+//     struct pairs *string2head = NULL;
+//     struct pairs *string2current;
+//     struct pairs *string2previous;
     
-    int counter=0, string1pairs=0, string2pairs=0;
+//     int counter=0, string1pairs=0, string2pairs=0;
     
-    if (string1[0]=='\0' || string2[0]=='\0')
-            return 0;
-    while (string1[counter+1] != '\0')
-    {
-	    if (string1[counter] != ' ' && string1[counter+1] != ' ')
-	            {
-	            string1current = (struct pairs *) malloc(sizeof(struct pairs));
-	            if (string1current==NULL)
-	                    exit(EXIT_FAILURE);
-	            if (string1head == NULL)      
-	                    string1head = string1current;
-	            else                   
-	                    string1previous->next = string1current;
-	            string1current->next = NULL;
-	            string1current->chars[0]=toupper(string1[counter]);
-	            string1current->chars[1]=toupper(string1[counter+1]);
-	            string1previous=string1current;
-	            string1pairs++;
-	            }
-	    counter++;
-    }
+//     if (string1[0]=='\0' || string2[0]=='\0')
+//             return 0;
+//     while (string1[counter+1] != '\0')
+//     {
+// 	    if (string1[counter] != ' ' && string1[counter+1] != ' ')
+// 	            {
+// 	            string1current = (struct pairs *) malloc(sizeof(struct pairs));
+// 	            if (string1current==NULL)
+// 	                    exit(EXIT_FAILURE);
+// 	            if (string1head == NULL)      
+// 	                    string1head = string1current;
+// 	            else                   
+// 	                    string1previous->next = string1current;
+// 	            string1current->next = NULL;
+// 	            string1current->chars[0]=toupper(string1[counter]);
+// 	            string1current->chars[1]=toupper(string1[counter+1]);
+// 	            string1previous=string1current;
+// 	            string1pairs++;
+// 	            }
+// 	    counter++;
+//     }
             
-    counter=0;      
-    while (string2[counter+1] != '\0')
-    {
-	    if (string2[counter] != ' ' && string2[counter+1] != ' ')
-	            {
-	            string2current = (struct pairs *) malloc(sizeof(struct pairs));
-	            if (string2current==NULL)
-	                    exit(EXIT_FAILURE);
-	            if (string2head == NULL)      
-	                    string2head = string2current;
-	            else                   
-	                    string2previous->next = string2current;
-	            string2current->next = NULL;
-	            string2current->chars[0]=toupper(string2[counter]);
-	            string2current->chars[1]=toupper(string2[counter+1]);
-	            string2previous=string2current;
-	            string2pairs++;
-	            }
-	    counter++;
-    }
-    counter=0;
-    string1current=string1head;
-    while (string1current != NULL)
-    {
-        string2current=string2head;
-        while (string2current != NULL)
-                {
-                if (string2current->chars[0]== string1current->chars[0] && string2current->chars[1]==string1current->chars[1])
-                        {
-                        string2current->chars[0]='\0'; // Otherwise, 'GGGGG' would score a perfect match against 'GG'.
-                        counter+=2;
-                        break;
-                        }
-                string2current=string2current->next;
-                }
-        string1current=string1current->next;
-    }
+//     counter=0;      
+//     while (string2[counter+1] != '\0')
+//     {
+// 	    if (string2[counter] != ' ' && string2[counter+1] != ' ')
+// 	            {
+// 	            string2current = (struct pairs *) malloc(sizeof(struct pairs));
+// 	            if (string2current==NULL)
+// 	                    exit(EXIT_FAILURE);
+// 	            if (string2head == NULL)      
+// 	                    string2head = string2current;
+// 	            else                   
+// 	                    string2previous->next = string2current;
+// 	            string2current->next = NULL;
+// 	            string2current->chars[0]=toupper(string2[counter]);
+// 	            string2current->chars[1]=toupper(string2[counter+1]);
+// 	            string2previous=string2current;
+// 	            string2pairs++;
+// 	            }
+// 	    counter++;
+//     }
+//     counter=0;
+//     string1current=string1head;
+//     while (string1current != NULL)
+//     {
+//         string2current=string2head;
+//         while (string2current != NULL)
+//                 {
+//                 if (string2current->chars[0]== string1current->chars[0] && string2current->chars[1]==string1current->chars[1])
+//                         {
+//                         string2current->chars[0]='\0'; // Otherwise, 'GGGGG' would score a perfect match against 'GG'.
+//                         counter+=2;
+//                         break;
+//                         }
+//                 string2current=string2current->next;
+//                 }
+//         string1current=string1current->next;
+//     }
                 
-    string1current = string1head;
-    while (string1current != NULL)
-    {
-        free(string1current);
-        string1current = string1current->next;
-    }
-    string2current = string2head;
-    while (string2current != NULL)
-    {
-        free(string2current);
-        string2current = string2current->next;
-    }
-    return (((double)counter/(string1pairs+string2pairs))*100);
-}
+//     string1current = string1head;
+//     while (string1current != NULL)
+//     {
+//         free(string1current);
+//         string1current = string1current->next;
+//     }
+//     string2current = string2head;
+//     while (string2current != NULL)
+//     {
+//         free(string2current);
+//         string2current = string2current->next;
+//     }
+//     return (((double)counter/(string1pairs+string2pairs))*100);
+// }
 
 size_t levenshtein_distance(std::vector<Instruction*> v1, std::vector<Instruction*> v2)
 {
